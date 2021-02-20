adda380f3a95c0b6b7a8e083a28669df67c103be
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-02-20 18:53:53 -0300

    Fixed Mongo connection problem

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,10 +5,22 @@ exports.sign_up_get = function(req, res, next) {
 }
 
 exports.sign_up_post = function(req, res, next) {
-
+    const user = new User(
+        {
+            first_name: req.body.firstname,
+            last_name: req.body.lastname,
+            username: req.body.username,
+            password: req.body.password,
+            status: 'member'
+        }
+    )
+    user.save((err) => {
+        if (err) { return next(err) }
+        res.redirect('/')
+    })
 }
 
-exports.log_in_post = function(req, res, next) {
+exports.log_in_get = function(req, res, next) {
     res.render('log_in', { title: 'Log in' });
 }
 
